e00f00ea19c37b5e73becd9eb83e4e6d65531567
Dima <mitsmirnov@gmail.com>
2017-10-15 17:14:52 +0300

    Create header php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Личный сайт студента Geekbrains</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+
+    <?php wp_head(); ?>
+</head>
--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <title>Личный сайт студента Geekbrains</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-
-    <?php wp_head(); ?>
-</head>
+<?php get_header(); ?>
 
 <body>
     <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
